7d327090981a10f7ff72396ab2a9ee715bfd63e2
Dmitry Ermakov <de@dimer.org.ua>
2022-01-30 21:48:26 +0300

    Add tftp alias

--- cmd/net.c
+++ cmd/net.c
@@ -36,6 +36,12 @@ int do_tftpb(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 }
 
 U_BOOT_CMD(
+	tftp,	3,	1,	do_tftpb,
+	"boot image via network using TFTP protocol",
+	"[loadAddress] [[hostIPaddr:]bootfilename]"
+);
+
+U_BOOT_CMD(
 	tftpboot,	3,	1,	do_tftpb,
 	"boot image via network using TFTP protocol",
 	"[loadAddress] [[hostIPaddr:]bootfilename]"
